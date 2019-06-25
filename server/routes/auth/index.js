27dd396548b0cb98b318b6e3d7d8d1a2eb0da5c2
Will-Copeland <will@localhost.localdomain>
2019-06-25 12:57:38 -0700

    sets session userID

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const User = require('../../schema');
 
 const router = express.Router();
 
+router.use(cookieParser());
+router.use(bodyParser.urlencoded());
 router.post('/register', (req, res) => {
   User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
     if (err) {
+      console.log('ERROR', err);
+
       return res.render('register', { user });
     }
 
     passport.authenticate('local')(req, res, () => {
-      res.redirect('/');
+      req.session.userID = user._id;
+      res.redirect('/dashboard');
     });
   });
 });
