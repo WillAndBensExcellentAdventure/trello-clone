d5b92befcad539dddd405700038bfc8486f930a2
Will-Copeland <will@localhost.localdomain>
2019-06-25 12:57:26 -0700

    added session test route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,12 @@ const routes = require('express').Router();
 const dashboard = require('./dashboards');
 const auth = require('./auth');
 
+routes.use('/auth', auth);
+
 routes.use('/dashboard', dashboard);
 
-routes.use('/auth', auth);
+routes.get('/test', (req, res) => {
+  console.log(req.session.userID);
+});
 
 module.exports = routes;
