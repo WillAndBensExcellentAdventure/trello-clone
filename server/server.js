31e1bd6ea898844870872cc0e543019e48c2b943
Will-Copeland <will@localhost.localdomain>
2019-06-27 23:24:38 -0700

    example routes with INSERT and SELECT

--- server/server.js
+++ server/server.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const { Pool } = require('pg');
+const uuid = require('uuid/v4');
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'trellio',
+  password: 'mypassword',
+  port: 5432,
+});
+
+const testDBInsert = (request, response) => {
+  const uud = uuid();
+  pool.query(`INSERT INTO users (ID, username, passwordHash) VALUES ('${uud}', 'testsname', '10w8221')`, (error, results) => {
+    if (error) console.log('INSERT ERROR', error);
+    
+    response.status(200).json(results);
+  });
+};
+
+const testDB = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY ID ASC', (error, results) => {
+    if (error) console.log('ERROR', error);
+    console.log(results);
+    response.status(200).json(results.rows);
+  });
+};
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("doxssd");
-});
+app.get('/', testDB);
+app.get('/insert', testDBInsert);
 
 const port = 8080;
 
