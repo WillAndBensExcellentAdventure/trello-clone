a158fa51274f95ba899bc88d4b5b53c9af264b5d
Will-Copeland <will@localhost.localdomain>
2019-06-29 22:35:09 -0700

    signup/login routes and test routes

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,53 @@
 const authHelpers = require('./config/authHelpers');
+const queries = require('./db/queries');
 
 
 module.exports = (app, passport, db) => {
-  app.get('/', (req, res) => {
-    res.send('hello');
+  app.get('/api', (req, res) => {
+    res.send('worl');
   });
 
-  app.post('/signup', (req, res, next) => authHelpers.createUser(req).then((results) => {
-    passport.authenticate('local', (error, user, info) => {
-      if (user) res.status(200).json({ status: info });
+  app.get('/api/test2', (req, res) => {
+    console.log('SES PASS', req.user);
+  });
+
+  app.post('/api/test', (req, res, next) => {
+    if (req.isAuthenticated()) {
+      res.send('authd');
+      next();
+    } else {
+      res.send('no auth');
+    }
+  });
+
+  app.post('/api/login', (req, res, next) => {
+    passport.authenticate('local', (err, user) => {
+      if (err) console.log(err);
+      if (user) {
+        req.login(user, (error) => {
+          console.log('logg');
+
+          if (error) console.log(error);
+        });
+        res.status(200).json({ status: 'success' });
+      }
     })(req, res, next);
-  }));
+  });
+
+
+  app.post('/api/signup', async (req, res, next) => {
+    await new Promise((resolve, reject) => {
+      authHelpers.createUser(req, resolve);
+    });
+
+    passport.authenticate('local', (err, user, info) => {
+      if (err) console.log(err);
+      if (user) {
+        req.login(user, (error) => {
+          if (error) console.log(error);
+        });
+        res.status(200).json({ status: 'success' });
+      }
+    })(req, res, next);
+  });
 };
