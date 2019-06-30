4271edbe048170aac19f6c4c282ea9eb29303daa
Will-Copeland <will@localhost.localdomain>
2019-06-29 22:34:41 -0700

    error messages, removed logs

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,22 +3,22 @@ const bcrypt = require('bcrypt');
 
 module.exports = (passport, db) => {
   passport.use(new LocalStrategy((username, password, cb) => {
-    db.query('SELECT id, username, passwordHash FROM users WHERE username=$1', [username], (err, result) => {
-      if (err) {
-        throw new Error(err);
+    db.query('SELECT ID, username, passwordHash FROM users WHERE username=$1', [username], (error, result) => {
+      if (error) {
+        throw new Error(error);
       }
 
       if (result.rows.length > 0) {
         const first = result.rows[0];
-        bcrypt.compare(password, first.passwordHash, (err, res) => {
+        bcrypt.compare(password, first.passwordhash, (err, res) => {
           if (res) {
             cb(null, { id: first.id, username: first.username });
           } else {
-            cb(null, false);
+            cb('password compare failed', false);
           }
         });
       } else {
-        cb(null, false);
+        cb('user not found', false);
       }
     });
   }));
@@ -27,13 +27,12 @@ module.exports = (passport, db) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, cb) => {
-    db.query('SELECT id, username FROM users WHERE id = $1', [id], (err, results) => {
+  passport.deserializeUser((id, done) => {
+    db.query('SELECT ID, username FROM users WHERE ID = $1', [id], (err, results) => {
       if (err) {
         throw new Error('Error when selecting user on session deserialize', err);
       }
-
-      cb(null, results.rows[0]);
+      done(null, results.rows[0]);
     });
   });
 };
