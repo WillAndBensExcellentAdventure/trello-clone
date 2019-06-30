a52ebcd68ebd95080589f3b080c5920139327a00
Will-Copeland <will@localhost.localdomain>
2019-06-29 22:36:25 -0700

    promise for db query

--- server/config/authHelpers.js
+++ server/config/authHelpers.js
@@ -3,15 +3,17 @@ const uuid = require('uuid/v4');
 const db = require('../db');
 
 module.exports = {
-  createUser(req) {
-    const hash = bcrypt.hashSync(req.body.password);
+  createUser(req, resolve) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const userID = uuid();
 
     return db.query(
       `INSERT INTO users (ID, username, passwordHash)
-           VALUES (${userID}, ${req.body.username}, ${hash}`,
+           VALUES ('${userID}', '${req.body.username}', '${hash}')`,
       (error, results) => {
-        if (error) return error;
+        if (error) return console.log(error);
+        resolve();
         return results;
       },
     );
