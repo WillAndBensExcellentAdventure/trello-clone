cf6b6a1fdf4ff1daa23e4c749c48164e421fe413
Will-Copeland <will@localhost.localdomain>
2019-06-29 22:35:32 -0700

    added testing queries

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,5 +1,31 @@
 const db = require('./index');
 
 module.exports = {
-    
-}
+  testDBInsert(req, res) {
+    const uud = uuid();
+    db.query(`INSERT INTO users (ID, username, passwordHash) VALUES ('${uud}', 'testsname', '10w8221')`, (error, results) => {
+      if (error) console.log('INSERT ERROR', error);
+
+      res.status(200).json({ results });
+    });
+  },
+
+  testDB(req, res) {
+    db.query('SELECT * FROM users ORDER BY ID ASC', (error, results) => {
+      if (error) res.status(400).json({ error, status: 'Query failed' });
+      res.status(200).json({ users: results.rows, sesh: req.session });
+    });
+  },
+
+  testQuery(req, res) {
+      console.log(req.body.username);
+      
+    db.query('SELECT ID, username, passwordHash FROM users WHERE username=$1', [req.body.username], (err, result) => {
+      res.json(result.rows);
+    });
+  },
+
+//   createDashboard(req, res) {
+//       const user = req.session.user
+//   }
+};
