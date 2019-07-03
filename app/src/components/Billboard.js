d5fa23029db49dd4bd7452c492d06d7c140805a3
Will-Copeland <will@localhost.localdomain>
2019-07-02 22:42:43 -0700

    color gradient

---
+++ app/src/components/Billboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+
+const styles = ({ breakpoints }) => ({
+  root: {
+    background: "linear-gradient(to bottom right, #0c76c0, #4669c4)",
+    // backgroundColor: 'black',
+    width: "100%",
+    height: "600px",
+    position: "absolute"
+  }
+});
+
+function Billboard(props) {
+  const { classes } = props;
+  return <div className={classes.root} />;
+}
+
+export default withStyles(styles)(Billboard);
