37fa00c9247d07eaf8df5257625751248199e47b
Will-Copeland <will@localhost.localdomain>
2019-07-02 22:42:29 -0700

    landing page route

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React, { PureComponent } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm";
 import Layout from "./util/Layout";
 // import Axios from 'axios';
 import Auth from "./util/Auth";
 import LoginForm from "./components/LoginForm";
+import Billboard from "./components/Billboard";
+import LandingPage from "./pages/LandingPage";
 
 class App extends PureComponent {
   renderJunk() {
@@ -18,8 +20,12 @@ class App extends PureComponent {
   render() {
     return (
       <Layout>
-        <Route path="/signup" component={SignUpForm} />
-        <Route path="/login" component={LoginForm} />
+        <Switch>
+          <Route path="/signup" component={SignUpForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/" component={LandingPage} />
+          {/* {this.renderJunk()} */}
+        </Switch>
       </Layout>
     );
   }
