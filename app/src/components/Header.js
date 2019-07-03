9a6e3e8c30b4c99bbd5125650e4052e320d0c025
Will-Copeland <will@localhost.localdomain>
2019-07-02 22:42:57 -0700

    header style adjustments

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -11,40 +11,68 @@ import { Link } from "react-router-dom";
 
 const styles = {
   root: {
+    position: "fixed",
     flexGrow: 1
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    textDecoration: "none",
+    color: "white"
+  },
+  link: {
+    textDecoration: "none",
+    color: "white"
+  },
+  button: {
+    fontFamily: "Arial"
   }
 };
 
 function Header(props) {
   function ElevationScroll(props) {
     const trigger = useScrollTrigger({
-      threshold: 0
+      threshold: 20,
+      disableHysteresis: true
     });
 
     return React.cloneElement(props.children, {
       elevation: trigger ? 4 : 0,
-      color: trigger ? "white" : "primary"
+      style: {
+        background: trigger
+          ? "#0079bf"
+          : "linear-gradient(to right, #0c76c0, #4669c4)"
+      }
     });
   }
 
   const { classes } = props;
   return (
-    <div className={classes.root}>
+    <div>
       <ElevationScroll>
-        <AppBar>
+        <AppBar className={classes.root}>
           <Toolbar>
             <Typography variant="h4" className={classes.title}>
-              Trellio
+              <Link className={classes.title} to="/">Trellio</Link>
             </Typography>
-            <Button>
-              <Link to="/login">Login</Link>
-            </Button>
 
-            <Button variant="contained" color="secondary">
-              <Link to="/signup">Signup</Link>
+            <Button className={classes.button}>
+              <Link className={classes.link} to="/login">
+                Login
+              </Link>
+            </Button>
+            <Button
+              component={Typography}
+              className={classes.button}
+              variant="contained"
+              color="#FFF"
+            >
+              <Link
+                style={{ color: "#4669c4" }}
+                className={classes.link}
+                to="/signup"
+              >
+                Signup
+              </Link>
             </Button>
           </Toolbar>
         </AppBar>
