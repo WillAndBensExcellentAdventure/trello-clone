9e559e5908bfe3ffbb328ee45b98a7461c5c2563
Will-Copeland <will@localhost.localdomain>
2019-07-06 21:26:16 -0700

    flex adjustments

--- app/src/components/Header/LoggedInHeader.js
+++ app/src/components/Header/LoggedInHeader.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { AppBar, Toolbar, withStyles } from "@material-ui/core";
+import { AppBar, Toolbar, withStyles, Typography } from "@material-ui/core";
 import LeftHandNav from "./LeftHandNav";
 
 const styles = ({ breakpoints }) => ({
   root: {
     // height: "40px"
+    flexGrow: 1
   },
   toolBar: {
-    minHeight: "32px"
+    minHeight: "32px",
+    flexGrow: 1,
+    justifyContent: "space-between"
   }
 });
 
 function LoggedInHeader(props) {
   const { classes } = props;
   return (
-    <div>
-      <AppBar className={classes.root}>
+    <div className={classes.root}>
+      <AppBar>
         <Toolbar disableGutters className={classes.toolBar} variant="dense">
           <LeftHandNav />
-          {/* Trellio */}
+          <Typography variant="subtitle1">Trellio</Typography>
           {/* <CreateInfoNotiAvatar /> */}
         </Toolbar>
       </AppBar>
