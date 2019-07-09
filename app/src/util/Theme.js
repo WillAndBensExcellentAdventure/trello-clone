fb4949322fb444c8948fadd37989cb5dfc590989
Will-Copeland <will@localhost.localdomain>
2019-07-09 09:12:14 -0700

    palette change

--- app/src/util/Theme.js
+++ app/src/util/Theme.js
@@ -1,13 +1,14 @@
 import { createMuiTheme } from '@material-ui/core';
-import { red, amber } from '@material-ui/core/colors';
+import { red, blue, indigo } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#0179bf",
+      dark: indigo[900]
     },
     secondary: {
-      main: red[500],
+      main: blue[700],
     },
   },
 });
